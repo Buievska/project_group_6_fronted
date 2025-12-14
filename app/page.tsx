636788d87a57c892aec6a71d0d21e73781cfe47b
Ania <anna111239@gmail.com>
2025-12-14 17:09:49 +0100

    page setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-
 import styles from "./page.module.css";
 
 import { HeroBlock } from "@/components/HeroBlock/HeroBlock";
-import FeaturedToolsBlock from "../components/FeaturedToolsBlock/FeaturedToolsBlock";
 import BenefitsBlock from "@/components/BenefitsBlock/BenefitsBlock";
+import FeaturedToolsBlock from "@/components/FeaturedToolsBlock/FeaturedToolsBlock";
 import { FeedbacksBlock } from "@/components/FeedbacksBlock/FeedbacksBlock";
 import { RegistrationBlock } from "@/components/RegistrationBlock/RegistrationBlock";
 
