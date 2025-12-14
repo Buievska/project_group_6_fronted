50e35cfc13d9c7840a0279c4b131925897293e81
OksanaSlonska <slonskayaoksana@gmail.com>
2025-12-14 14:56:49 +0200

    add HeroBlock

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import styles from "./page.module.css";
 
-import { HeroBlock } from "@/components/HeroBlock/HeroBlock";
 import FeaturedToolsBlock from "../components/FeaturedToolsBlock/FeaturedToolsBlock";
 import BenefitsBlock from "@/components/BenefitsBlock/BenefitsBlock";
 import { FeedbacksBlock } from "@/components/FeedbacksBlock/FeedbacksBlock";
 import { RegistrationBlock } from "@/components/RegistrationBlock/RegistrationBlock";
+import HeroBlock from "@/components/HeroBlock/HeroBlock";
 
 export default function HomePage() {
   return (
