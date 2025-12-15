ae9820522d7078bc23cbe8c3a4afba90fcd00504
Ania <anna111239@gmail.com>
2025-12-15 23:44:35 +0100

    fix css registration block

--- components/RegistrationBlock/RegistrationBlock.tsx
+++ components/RegistrationBlock/RegistrationBlock.tsx
@@ -1,26 +1,32 @@
 import Link from "next/link";
-import Image from 'next/image';
-import css from "./RegistrationBlock.module.css"
-
+import Image from "next/image";
+import css from "./RegistrationBlock.module.css";
 
 export const RegistrationBlock = () => {
   return (
-    <section className={css.container}>
-      <div className={css.content}>
-        <h2 className={css.registrationBlockTitle}>Зареєструйтесь і отримайте доступ до інструментів поруч із вами</h2>
-        <p className={css.registrationBlockSubtitle}>Не витрачайте гроші на купівлю — орендуйте зручно та швидко.<br />
-        Приєднуйтесь до ToolNext вже сьогодні!</p>
-        <Link href="/auth/register" className={css.btnPrimary}>
-          Зареєструватися
-        </Link>
+    <section className={css.registrationBlockSection}>
+      <div className={css.container}>
+        <div className={css.content}>
+          <h2 className={css.registrationBlockTitle}>
+            Зареєструйтесь і отримайте доступ до інструментів поруч із вами
+          </h2>
+          <p className={css.registrationBlockSubtitle}>
+            Не витрачайте гроші на купівлю — орендуйте зручно та швидко.
+            <br />
+            Приєднуйтесь до ToolNext вже сьогодні!
+          </p>
+          <Link href="/register" className={css.btnPrimary}>
+            Зареєструватися
+          </Link>
+        </div>
+        <Image
+          className={css.registrationBlockImage}
+          src="/img/imgRegBlock.png"
+          alt="Tools"
+          width={335}
+          height={223}
+        />
       </div>
-      <Image
-        className={css.registrationBlockImage}
-        src="/img/imgRegBlock.png"
-        alt="Tools"
-        width={335}
-        height={223}
-      />
     </section>
   );
 };
