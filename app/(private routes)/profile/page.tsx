e9eb3040f7fb2124be803531e134b259d6f74339
Ania <anna111239@gmail.com>
2025-12-29 17:56:24 +0100

    fix

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
 import { getCurrentUser } from "@/lib/api/clientApi";
 import { useAuthStore } from "@/lib/store/authStore";
 import styles from "./MyProfilePage.module.css";
@@ -10,7 +9,6 @@ import styles from "./MyProfilePage.module.css";
 export default function MyProfilePage() {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(true);
-
   const loginToStore = useAuthStore((state) => state.login);
 
   useEffect(() => {
@@ -23,14 +21,25 @@ export default function MyProfilePage() {
           loginToStore(user);
 
           const userId = user._id || user.id;
+
           router.push(`/profile/${userId}`);
+        } else {
+          router.push("/login");
         }
       } catch (error) {
         console.error("Не вдалося авторизуватись:", error);
-        setIsLoading(false);
+        router.push("/login");
+      } finally {
       }
     };
 
     checkAuth();
   }, [router, loginToStore]);
+
+  return (
+    <div className={styles.loaderContainer}>
+      <div className={styles.spinner}></div>
+      <p className={styles.loaderText}>Завантаження профілю...</p>
+    </div>
+  );
 }
